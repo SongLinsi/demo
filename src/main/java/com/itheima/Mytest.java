6a3a47ca99d5913d409c09ead61ddc36f3574904
981583970@qq.com <981583970@qq.com>
2018-11-24 20:48:07 +0800

    放假时间都会

--- src/main/java/com/itheima/Mytest.java
+++ src/main/java/com/itheima/Mytest.java
@@ -6,8 +6,6 @@ public class Mytest {
     public static void main(String[] args) {
         System.out.println("lallala");
         System.out.println("lzj");
-        System.out.println("weishenme");
-        System.out.println("1231321321312");
-        System.out.println("分别个");
+      
     }
 }
