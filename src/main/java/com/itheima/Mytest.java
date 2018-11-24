e416403f73dfdd335187c1206886ab9b77b5b811
981583970@qq.com <981583970@qq.com>
2018-11-24 20:14:28 +0800

    提交到本地

---
+++ src/main/java/com/itheima/Mytest.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Mytest {
+    public static void main(String[] args) {
+        System.out.println("lallala");
+        System.out.println("lzj");
+        System.out.println("weishenme");
+        System.out.println("分别个");
+    }
+}
